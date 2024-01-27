a79c3526cd4ab822869a4dbb5816b68ac584a2ba
PratikB131 <137922353+PratikB131@users.noreply.github.com>
2024-01-27 19:00:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/public")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -34,7 +34,7 @@ app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //PORT
